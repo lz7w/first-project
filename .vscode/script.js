4eb31ef48b0384bfd282d983a2ccb242f4d2bb93
Luiz Fernando <liizfernandolopesdeoliveira@gmail.com>
2023-10-15 12:52:41 -0300

    fist commit

---
+++ .vscode/script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    html.classList.toggle('light')
+
+    //pegar a tag img
+    const img = document.querySelector("#profile img")
+    if(html.classList.contains('light')) {
+        img.setAttribute('src', './assets/avatar-light.png')
+    }
+    else{
+        img.setAttribute('src', './assets/avatar.png')
+    }
+
+    
+    //substituir a imagem
+    //se tiver em light mode, adicionar a imagem light
+    //se tiver dark mode, manter imagem normal
+}
